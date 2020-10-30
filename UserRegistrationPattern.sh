095f65a5e4f85f1c53908fa9ecb67ee21915863e
aakash2809 <aakashrajak289@gmail.com>
2020-10-30 23:07:28 +0530

    Add : first name validation

--- UserRegistrationPattern.sh
+++ UserRegistrationPattern.sh
@@ -1,4 +1,23 @@
-#! /bin/bash -x
+#! /bin/bash 
 
 echo "              Welcome to User Registration Program"
 echo "---------------------------------------------------------------------"
+
+#TO VALIDATE PATTERN
+validate(){
+    local input=$1
+    local pattern=$2
+
+    if [[ $input =~ $pattern ]]
+    then
+        echo "Valid"
+    else
+        echo "Invalid"
+    fi   
+}
+
+
+read -p "Enter the first name : " firstName
+firstNamePattern='^[A-Z]{1}[a-zA-Z]{2,}$'
+result=$(validate $firstName $firstNamePattern)
+echo  $result
