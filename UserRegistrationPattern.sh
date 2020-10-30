5488a8d3fa4e55341e66176c81035d6ceba1a598
aakash2809 <aakashrajak289@gmail.com>
2020-10-31 02:46:12 +0530

    Add : Email samples check

--- UserRegistrationPattern.sh
+++ UserRegistrationPattern.sh
@@ -1,4 +1,21 @@
-#! /bin/bash
+#! /bin/bash -x
+
+#DECLARATION OF ARRAY FOR SAMPLE EMAILS
+declare -a validEmailSample
+declare -a invalidEmailSample
+
+#ASSIGING SOME VALID SAMPLES
+validEmailSample=('abc@yahoo.com' 'abc-100@yahoo.com'
+            'abc.100@yahoo.com' 'abc111@abc.com'
+            'abc-100@abc.net' 'abc.100@abc.com.au'
+            'abc@1.com' 'abc@gmail.com.com' 'abc+100@gmail.com')
+
+#ASSIGING SOME VALID SAMPLES
+invalidEmailSample=('abc' 'abc@.com.my' 'abc123@gmail.a' 'abc123@.com'
+                  'abc123@.com.com' '.abc@abc.com' 'abc()*@gmail.com'
+                   'abc@%*.com' 'abc..2002@gmail.com' 'abc.@gmail.com'
+                   'abc@abc@gmail.com' 'abc@gmail.com.1a' 'abc@gmail.com.aa.au')
+
 
 echo "              Welcome to User Registration Program"
 echo "---------------------------------------------------------------------"
@@ -45,3 +62,23 @@ read -p "Enter the password :" password
 passwordPattern='^(?=.*[0-9])(?=.*[A-Z])(?=.*[~!.+?_><!@#$%^&*])[a-zA-Z0-9!?.+-~!@#$%^&*_]{8,}$'
 result=$(validate $password $passwordPattern)
 echo  $result
+
+
+#VALIDATION BY ITERATING SAMPPLE EMAIL
+length1=${#validEmailSample[@]}
+index=0
+while [ $index -lt $length1 ]
+do
+   result=$(validate ${validEmailSample[$index]} $emailPattern)
+   echo $result
+	((index++))
+done
+
+length2=${#invalidEmailSample[@]}
+index=0
+while [ $index -lt $length2 ]
+do
+   result=$(validate ${invalidEmailSample[$index]} $emailPattern)
+   echo $result
+	((index++))
+done
